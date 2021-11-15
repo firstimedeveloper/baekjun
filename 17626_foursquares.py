ffc47d316a6468d9321b8f437a0e699ee223b723
Jun Hyuk Han <hanjun.1998@gmail.com>
2021-11-16 01:41:58 +0900

    implemented a bruteforth algorithm for finding foursquares in python

---
+++ 17626_foursquares.py
@@ -0,0 +1,20 @@
+def solve(n):
+    if (n ** 0.5).is_integer():
+        return 1
+    for i in range(1, int(n ** 0.5 + 1)):
+        if n < i ** 2:
+            break
+        if ((n - i ** 2) ** 0.5).is_integer():
+            return 2
+    for i in range(1, int(n ** 0.5 + 1)):
+        if n < i ** 2:
+            break
+        for j in range(1, int((n - i ** 2) ** 0.5) + 1):
+            if n < i ** 2 + j ** 2:
+                break
+            if ((n - i ** 2 - j ** 2) ** 0.5).is_integer():
+                return 3
+    return 4
+
+
+print(solve(int(input())))
