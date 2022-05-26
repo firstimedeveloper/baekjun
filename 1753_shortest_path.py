881a55d494260e26854337e82056c39fc1d9b167
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-05-27 00:14:44 +0900

    passing, implemented djikstras shortest path algo

--- 1753_shortest_path.py
+++ 1753_shortest_path.py
@@ -1,14 +1,16 @@
-import collections, queue
+import collections, queue, sys
+input = sys.stdin.readline
 
-def solution(n, edges, start):
+def solution(E, start):
     graph = collections.defaultdict(list)
-    for u, v, w in edges:
+    for _ in range(E):
+        u, v, w = map(int, input().split())
         graph[u].append((v, w))
 
     paths = dict()
     q = queue.PriorityQueue()
     q.put((0, start))
-    while q:
+    while not q.empty():
         w, v = q.get()
         if v not in paths:
             paths[v] = w
@@ -20,11 +22,9 @@ def solution(n, edges, start):
 
 V, E = map(int, input().split())
 start = int(input())
-edges = []
-for _ in range(E):
-    u, v, w = map(int, input().split())
-    edges.append((u, v, w))
-for i in edges:
-    print(i)
-paths = solution(V, edges, start)
-print(paths)
+paths = solution(E, start)
+for i in range(1, V+1):
+    if i in paths:
+        print(paths[i])
+    else:
+        print("INF")
